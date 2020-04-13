d44289d880ea031296d7ba65c93580cc37be87ab
Ivan Tyshchenko <TihonV@users.noreply.github.com>
2020-04-13 10:55:58 +0300

    Update build.sh

--- 4_parallel_execution/build.sh
+++ 4_parallel_execution/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+PYTHON_PREFIX=`python3-config --prefix`
+export PKG_CONFIG_PATH=$PYTHON_PREFIX/lib/pkgconfig/
+
 rm *.so
 rm perf.h
 rm perf.c
@@ -8,4 +11,4 @@ go build -buildmode=c-shared -o perf.so *.go
 
 python pygen.py > perf.c
 
-gcc perf.c -dynamiclib perf.so -o pyperf.so `pkg-config --cflags --libs python3` `python3-config --libs --embed`
+gcc perf.c -dynamiclib perf.so -o pyperf.so `pkg-config --cflags --libs python-3-embed`
