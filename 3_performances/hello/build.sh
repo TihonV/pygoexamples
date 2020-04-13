b5d9705b4d01b6b191961942cef33edef9af3e72
Ivan Tyshchenko <TihonV@users.noreply.github.com>
2020-04-13 10:54:58 +0300

    Update build.sh

--- 3_performances/hello/build.sh
+++ 3_performances/hello/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+PYTHON_PREFIX=`python3-config --prefix`
+export PKG_CONFIG_PATH=$PYTHON_PREFIX/lib/pkgconfig/
+
 rm *.so
 rm *.h
 rm *.a
 
 go build -buildmode=c-archive -o libhello.a hello.go
 
-gcc hello.c -shared -o hello.so `pkg-config --cflags --libs python3` `python3-config --libs --embed` -L . -lhello
+gcc hello.c -shared -o hello.so `pkg-config --cflags --libs python-3-embed` -L . -lhello
