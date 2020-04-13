0dc718814c99227a6fae2e22c4649d3edc54c9ed
Ivan Tyshchenko <TihonV@users.noreply.github.com>
2020-04-13 10:52:02 +0300

    Update build.sh

--- 1_from_c_to_python/build.sh
+++ 1_from_c_to_python/build.sh
@@ -5,7 +5,7 @@ DEFAULT_PKG_CONFIG_PATH=$PKG_CONFIG_PATH
 PYTHON_PREFIX=`python3-config --prefix`
 export PKG_CONFIG_PATH=$PYTHON_PREFIX/lib/pkgconfig/
 
-gcc -shared -o newmath.so `pkg-config --cflags --libs python-3.8-embed` newmath.c
+gcc -shared -o newmath.so `pkg-config --cflags --libs python-3-embed` newmath.c
 
 # Restore PKG CONFIG
 export PKG_CONFIG_PATH=$DEFAULT_PKG_CONFIG_PATH
