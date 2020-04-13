247cbbc6fd4361a5b7152872de5c7920a2820ac9
Ivan Tyshchenko <TihonV@users.noreply.github.com>
2020-04-13 10:54:29 +0300

    Update build.sh

--- 3_performances/newmath/build.sh
+++ 3_performances/newmath/build.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+PYTHON_PREFIX=`python3-config --prefix`
+export PKG_CONFIG_PATH=$PYTHON_PREFIX/lib/pkgconfig/
+
 rm *.so
 rm *.h
 rm *.a
 
 go build -buildmode=c-archive -o libnewmath.a
 
-gcc _newmath.c -shared -o newmath.so `pkg-config --cflags --libs python3` `python3-config --libs --embed` -L . -lnewmath
+gcc _newmath.c -shared -o newmath.so `pkg-config --cflags --libs python-3-embed` -L . -lnewmath
