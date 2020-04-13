f7127f3ef35db35d970d9ebde209991e50769de1
Ivan Tyshchenko <TihonV@users.noreply.github.com>
2020-04-13 10:53:11 +0300

    Update build.sh

--- 2_from_go_to_python/build.sh
+++ 2_from_go_to_python/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+DEFAULT_PKG_CONFIG_PATH=$PKG_CONFIG_PATH
+PYTHON_PREFIX=`python3-config --prefix`
+export PKG_CONFIG_PATH=$PYTHON_PREFIX/lib/pkgconfig/
+
 go build -buildmode=c-shared -o newmath.so
+
+# Restore PKG CONFIG
+DEFAULT_PKG_CONFIG_PATH=$PKG_CONFIG_PATH
